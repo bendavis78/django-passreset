fe15f353349193f49df4030d65b45567de7ac8f8
Ben Davis <bendavis78@gmail.com>
2012-11-04 17:19:58 -0600

    Fixed namespace for lazy-reversed urls in urls_ns

--- passreset/__init__.py
+++ passreset/__init__.py
@@ -1,21 +1,20 @@
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 from django.conf import settings
 from django.conf.urls.defaults import patterns,  url
-from django.utils.functional import lazy
 from django.contrib import admin
 from passreset.forms import passreset_form
 
 # override default admin login template
 admin.site.login_template = 'passreset/admin/login.html'
 
-def urls_ns(namespace=None, login_url=None, tpl_path=None):
+def urls_ns(namespace, login_url=None, tpl_path=None):
     """
     Creates namespaced urlpatterns with the given namespace
     """
     if login_url is None:
         login_url = settings.LOGIN_URL
-    post_reset_redirect_done = lazy(reverse, unicode)('passreset:password-reset-done')
-    post_reset_redirect_complete = lazy(reverse, unicode)('passreset:password-reset-complete')
+    post_reset_redirect_done = reverse_lazy('{}:password-reset-done'.format(namespace))
+    post_reset_redirect_complete = reverse_lazy('{}:password-reset-complete'.format(namespace))
     password_reset_form = passreset_form(namespace) 
     if tpl_path is None:
         tpl_path = namespace is not None and namespace.replace('-', '_') or 'registration'
@@ -49,5 +48,5 @@ def urls_ns(namespace=None, login_url=None, tpl_path=None):
                         'current_app': namespace}),
     ), 'passreset', namespace)
 
-urls = urls_ns()
+urls = urls_ns('passreset')
 admin_urls = urls_ns('admin-passreset', 'admin:login')
