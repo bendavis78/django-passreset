fcba382fe87a8ae9d81414e36ae76021400b2c0c
Ben Davis <bendavis78@gmail.com>
2012-11-04 13:53:52 -0600

    added ability to supply a template path for namespaced passreset setup

--- passreset/__init__.py
+++ passreset/__init__.py
@@ -8,7 +8,7 @@ from passreset.forms import passreset_form
 # override default admin login template
 admin.site.login_template = 'passreset/admin/login.html'
 
-def urls_ns(namespace=None, login_url=None):
+def urls_ns(namespace=None, login_url=None, tpl_path=None):
     """
     Creates namespaced urlpatterns with the given namespace
     """
@@ -17,23 +17,35 @@ def urls_ns(namespace=None, login_url=None):
     post_reset_redirect_done = lazy(reverse, unicode)('passreset:password-reset-done')
     post_reset_redirect_complete = lazy(reverse, unicode)('passreset:password-reset-complete')
     password_reset_form = passreset_form(namespace) 
+    if tpl_path is None:
+        tpl_path = namespace is not None and namespace.replace('-', '_') or 'registration'
+    tpl = lambda name: [
+        '{}/{}'.format(tpl_path, name),
+        'registration/{}'.format(name)
+    ]
     return (patterns(
         'django.contrib.auth.views', 
         url(r'^$', 'password_reset', 
                 name='password-reset',
                 kwargs={'post_reset_redirect': post_reset_redirect_done,
                         'password_reset_form': password_reset_form,
+                        'template_name': tpl('password_reset_form.html'),
+                        'email_template_name': tpl('password_reset_email.html'),
+                        'subject_template_name': tpl('password_reset_subject.txt'),
                         'current_app': namespace}), 
         url(r'^done/$', 'password_reset_done', 
                 name='password-reset-done', 
-                kwargs={'current_app': namespace}),
+                kwargs={'template_name': tpl('password_reset_done.html'),
+                        'current_app': namespace}),
         url(r'^confirm/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$', 'password_reset_confirm',
                 name='password-reset-confirm',
                 kwargs={'post_reset_redirect': post_reset_redirect_complete,
+                        'template_name': tpl('password_reset_confirm.html'),
                         'current_app': namespace}),
         url(r'^complete/$', 'password_reset_complete',
                 name='password-reset-complete',
                 kwargs={'extra_context':{'login_url':login_url},
+                        'template_name': tpl('password_reset_complete.html'),
                         'current_app': namespace}),
     ), 'passreset', namespace)
 
